d1be99c06c26d5b9d16085016780cecee29be027
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2021-12-20 18:59:38 -0300

    Consumer somente aonde será modificado

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -6,7 +6,7 @@ import 'package:provider/provider.dart';
 class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(context, listen: false);
 
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
@@ -25,7 +25,7 @@ class ProductItem extends StatelessWidget {
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
+          leading: Consumer<Product>(builder: (ctx, product, _) => IconButton(
             onPressed: () {
               product.toogleFavorite();
               print("Clicou em Favoritar Item");
@@ -33,7 +33,7 @@ class ProductItem extends StatelessWidget {
             icon: Icon(
                 product.isFavorite ? Icons.favorite : Icons.favorite_border),
             color: Theme.of(context).accentColor,
-          ),
+          )),
           title: Text(
             product.title,
             textAlign: TextAlign.center,
