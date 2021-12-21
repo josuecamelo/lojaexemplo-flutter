9c3ccf8178a8233398a924dbfe4bee9d7821631c
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2021-12-20 22:24:21 -0300

    Filtrando Favoritos

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -4,8 +4,26 @@ import 'package:lojaexemplo/models/product.dart';
 
 class ProductList with ChangeNotifier{
   List<Product> _items = dummyProducts;
+  bool _showFavoriteOnly = false;
 
-  List<Product> get items => [..._items]; //clonando lista para não retornar a referencia e sim um clone
+  //List<Product> get items => [..._items]; //clonando lista para não retornar a referencia e sim um clone
+
+  List<Product> get items{
+    if(_showFavoriteOnly){
+      return _items.where((prod) => prod.isFavorite).toList();
+    }
+    return [..._items];
+  }
+
+  void showFavoriteOnly(){
+    _showFavoriteOnly = true;
+    notifyListeners();
+  }
+
+  void showAll(){
+    _showFavoriteOnly = false;
+    notifyListeners();
+  }
 
   void addProduct(Product product){
     _items.add(product);
